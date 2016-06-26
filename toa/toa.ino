5e626c109e2534b990e7136e6d1ed95d74ad9f0b
Christian Nilsson <nikize@gmail.com>
2016-06-27 00:45:52 +0200

    Try a more dynamic configuration for pins

--- toa/toa.ino
+++ toa/toa.ino
@@ -12,13 +12,15 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPXPIN, NEO_GRB + NEO_
 
 int delayval = 200;  // Delay for a period of time (in milliseconds).
 unsigned long timeChanged = 0;
-int toa1PreviousState = HIGH;
-int toa2PreviousState = HIGH;
-uint32_t oldPixColor = 0;
+#define NUMPINS 2
+const int toaPins[NUMPINS] = {8, 9};
+bool prevState[NUMPINS] = {HIGH, HIGH};
+// 3 hours in millis
+const unsigned long TIMEOUT = (3 * 60 * 60 * 1000L);
 
 void setup() {
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
+  for (int i = 0; i < NUMPINS; i++)
+    pinMode(toaPins[i], INPUT_PULLUP);
   pixels.begin();
 }
 
@@ -26,23 +28,33 @@ void loop() {
   unsigned long currentTime = millis();
 
   // get lock status LOW if occupied, or HIGH if free
-  int toa1 = digitalRead(8);
-  int toa2 = digitalRead(9);
+  bool toa[NUMPINS];
+  bool allFree = true;
+  bool noneFree = true;
+  for (int i = 0; i < NUMPINS; i++) {
+    toa[i] = digitalRead(toaPins[i]);
 
-  if (toa1 != toa1PreviousState || toa2 != toa2PreviousState) {
-    timeChanged = currentTime;
-    toa1PreviousState = toa1;
-    toa2PreviousState = toa2;
+    if (!toa[i])
+      allFree = false;
+
+    if (toa[i])
+      noneFree = false;
+
+    if (toa[i] != prevState[i]) {
+      // state changed      
+      timeChanged = currentTime;
+      prevState[i] = toa[i];
+    }
   }
 
-  // Timed out after 3h, lower brightness
-  bool timeout = (currentTime - timeChanged > (3 * 60 * 60 * 1000));
-  uint32_t pixColor = toa1 && toa2 ?
+  // Timed out, lower brightness
+  bool timeout = (currentTime - timeChanged > TIMEOUT);
+  uint32_t pixColor = allFree ?
     pixels.Color(0, timeout ? 1 : 150, 0) :
-    (!toa1 && !toa2 ? pixels.Color(255, 0, 0) :
+    (noneFree ? pixels.Color(255, 0, 0) :
     pixels.Color(timeout ? 4 : 64, timeout ? 4 : 64, 0) );
 
-  if (pixColor != oldPixColor) {
+  if (pixColor != pixels.getPixelColor(0)) {
     pixels.setPixelColor(0, pixColor);
     pixels.show();
   }
