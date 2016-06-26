9696ab5408995571c86d2afbb40a75682d8d7fca
Ramis <christoffer@ramis.se>
2016-06-27 00:05:03 +0200

    Improved Monitor-page

---
+++ OccupancyService/OccupancyService/index.js
@@ -0,0 +1,33 @@
+﻿function RoomsViewModel() {
+    this.rooms = ko.observableArray([]);
+    this.connectionStatus = ko.observable("Connecting...");
+}
+
+var viewModel = new RoomsViewModel();
+ko.applyBindings(viewModel);
+
+// Set up SignalR-connection
+var occupancyHub = $.connection.occupancyHub;
+occupancyHub.client.occupancyChanged = function (roomId, isOccupied) {
+    for (var i = 0; i < viewModel.rooms().length; i++) {
+        var room = viewModel.rooms()[i];
+        if (room.Id === roomId) {
+            // Update isOccupied
+            var updatedRoom = {Id: roomId, Description: room.Description, IsOccupied: isOccupied};
+            viewModel.rooms.replace(room, updatedRoom);
+        }
+    }
+};
+
+$.get("Rooms")
+  .done(function (data) {
+      viewModel.rooms(data);
+
+      // Start the connection.
+      $.connection.hub.start().done(function () {
+          viewModel.connectionStatus("Connected");
+      });
+  })
+  .fail(function () {
+      viewModel.connectionStatus("No response from server");
+  });
