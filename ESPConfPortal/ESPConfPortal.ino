715ce423fdda35b6cdec5ec8e33f300a6ab7db13
Christian Nilsson <nikize@gmail.com>
2016-06-28 01:22:58 +0200

    Include the basic "toa" input checks and pixel set based on occupancy

--- ESPConfPortal/ESPConfPortal.ino
+++ ESPConfPortal/ESPConfPortal.ino
@@ -39,6 +39,16 @@
 #define NUMPIXELS      8
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPXPIN, NEO_GRB + NEO_KHZ800);
 
+/* --- toa ---- */
+const int delayval = 200;  // Delay for a period of time (in milliseconds).
+unsigned long timeChanged = 0;
+#define NUMPINS 2
+const int toaPins[NUMPINS] = {2, 3};
+bool prevState[NUMPINS] = {HIGH, HIGH};
+// 3 hours in millis
+const unsigned long TIMEOUT = (3 * 60 * 60 * 1000L);
+/* --- /toa --- */
+
 ESP8266WebServer http(80);
 ESP8266HTTPUpdateServer httpUpdater(true);
 
@@ -373,9 +383,60 @@ void setup() {
   DBG_OUTPUT_PORT.println("HTTP server started");
   setSyncProvider(getNtpTime);
   setSyncInterval(30 * 60);
+  /* --- toa ---- */
+  // Ensure we are note used to something else
+  //pinMode(3, FUNCTION3);
+  for (int i = 0; i < NUMPINS; i++)
+    pinMode(toaPins[i], INPUT_PULLUP);
+  /* --- /toa --- */
 }
+
+bool checkInput() {
+  unsigned long currentTime = millis();
+
+  // get lock status LOW if occupied, or HIGH if free
+  bool toa[NUMPINS];
+  bool allFree = true;
+  bool noneFree = true;
+  for (int i = 0; i < NUMPINS; i++) {
+    toa[i] = digitalRead(toaPins[i]);
+
+    if (!toa[i])
+      allFree = false;
+
+    if (toa[i])
+      noneFree = false;
+
+    if (toa[i] != prevState[i]) {
+      // state changed      
+      timeChanged = currentTime;
+      prevState[i] = toa[i];
+    }
+  }
+
+  // Timed out, lower brightness
+  bool timeout = (currentTime - timeChanged > TIMEOUT);
+  uint32_t pixColor = allFree ?
+    pixels.Color(0, timeout ? 1 : 150, 0) :
+    (noneFree ? pixels.Color(255, 0, 0) :
+    pixels.Color(timeout ? 4 : 64, timeout ? 4 : 64, 0) );
+
+  if (pixColor != pixels.getPixelColor(0)) {
+    pixels.setPixelColor(0, pixColor);
+    pixels.show();
+  }
+
+  return timeChanged == currentTime;
+}
+
+unsigned long lastInputCheck = 0;
 
 void loop() {
   http.handleClient();
   ArduinoOTA.handle();
+
+  if (millis() - lastInputCheck >= delayval) {
+    bool stateChanged = checkInput();
+    lastInputCheck = millis();
+  }
 }
