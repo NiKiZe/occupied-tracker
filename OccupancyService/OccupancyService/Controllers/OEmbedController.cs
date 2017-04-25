b5ab900e271202ecd14cbcbe55671e854c9c4fb5
Ramis <christoffer@ramis.se>
2017-04-25 10:06:11 +0200

    oEmbed WIP

---
+++ OccupancyService/OccupancyService/Controllers/OEmbedController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Web.Http;
+using System.Xml.Serialization;
+using OccupancyService.Models;
+
+namespace OccupancyService.Controllers
+{
+    /// <summary>
+    /// Implementation of a oEmber provider (http://oembed.com/)
+    /// </summary>
+    public class OEmbedController : ApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">Required. The URL to retrieve embedding information for.</param>
+        /// <param name="maxwidth">Optional. The maximum width of the embedded resource. Only applies to some resource types (as specified below). For supported resource types, this parameter must be respected by providers.</param>
+        /// <param name="maxheight">Optional. The maximum height of the embedded resource. Only applies to some resource types (as specified below). For supported resource types, this parameter must be respected by providers.</param>
+        /// <param name="format">Optional. 'json' or 'xml'. The required response format. When not specified, the provider can return any valid response format. When specified, the provider must return data in the request format, else return an error (see below for error codes).</param>
+        /// <returns></returns>
+        public HttpResponseMessage Get(string url = null, int? maxwidth = null, int? maxheight = null, string format = null)
+        {
+            if (format != "json" && format != "xml")
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotImplemented);
+            }
+
+            var oembed = new OEmbed
+            {
+                Type = "rich",
+                Version = "1.0",
+                Title = "Toalett-status",
+                ProviderName = "Caspeco Occupancy",
+                ProviderUrl = "http://caspecooccupancy.azurewebsites.net/",
+                Html = @"
+<ul>
+    <li>Test 1</li>
+    <li>Test 2</li>
+</ul>",
+                Width = 400,
+                Height = 300
+            };
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            switch (format)
+            {
+                case "json":
+                    var stream = new MemoryStream();
+                    var jsonSerializer = new DataContractJsonSerializer(typeof(OEmbed));
+                    jsonSerializer.WriteObject(stream, oembed);
+                    stream.Position = 0;
+                    var sr = new StreamReader(stream);
+                    response.Content = new StringContent(sr.ReadToEnd());
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    break;
+                case "xml":
+                    var xmlSerializerNamespaces = new XmlSerializerNamespaces();
+                    xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+                    var xmlSerializer = new XmlSerializer(typeof(OEmbed));
+                    StringWriter sw = new StringWriter();
+                    xmlSerializer.Serialize(sw, oembed, xmlSerializerNamespaces);
+                    response.Content = new StringContent(sw.ToString());
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
+                    break;
+            }
+            return response;
+        }
+    }
+}
---
+++ OccupancyService/OccupancyService/Models/OEmbed.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace OccupancyService.Models
+{
+    [DataContract(Name = "oembed")]
+    [XmlRoot("oembed")]
+    public class OEmbed
+    {
+        /// <summary>
+        /// Required. The resource type. Valid values, along with value-specific parameters, are described below.
+        /// photo - This type is used for representing static photos.
+        /// video - This type is used for representing playable videos.
+        /// link - Responses of this type allow a provider to return any generic embed data (such as title and author_name), without providing either the url or html parameters. The consumer may then link to the resource, using the URL specified in the original request.
+        /// rich - This type is used for rich HTML content that does not fall under one of the other categories
+        /// </summary>
+        [DataMember(Name = "type", EmitDefaultValue = false)]
+        [XmlElement("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The oEmbed version number. This must be 1.0.
+        /// </summary>
+        [DataMember(Name = "version", EmitDefaultValue = false)]
+        [XmlElement("version")]
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Optional. A text title, describing the resource.
+        /// </summary>
+        [DataMember(Name = "title", EmitDefaultValue = false)]
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Optional. The name of the author/owner of the resource.
+        /// </summary>
+        [DataMember(Name = "author_name", EmitDefaultValue = false)]
+        [XmlElement("author_name")]
+        public string AuthorName { get; set; }
+
+        /// <summary>
+        /// Optional. A URL for the author/owner of the resource.
+        /// </summary>
+        [DataMember(Name = "author_url", EmitDefaultValue = false)]
+        [XmlElement("author_url")]
+        public string AuthorUrl { get; set; }
+
+        /// <summary>
+        /// Optional. The name of the resource provider.
+        /// </summary>
+        [DataMember(Name = "provider_name", EmitDefaultValue = false)]
+        [XmlElement("provider_name")]
+        public string ProviderName { get; set; }
+
+        /// <summary>
+        /// Optional. The url of the resource provider.
+        /// </summary>
+        [DataMember(Name = "provider_url", EmitDefaultValue = false)]
+        [XmlElement("provider_url")]
+        public string ProviderUrl { get; set; }
+
+        /// <summary>
+        /// Optional. The suggested cache lifetime for this resource, in seconds. Consumers may choose to use this value or not.
+        /// </summary>
+        [DataMember(Name = "cache_age", EmitDefaultValue = false)]
+        [XmlElement("cache_age")]
+        public int CacheAge { get; set; }
+
+        /// <summary>
+        /// Optional. A URL to a thumbnail image representing the resource.
+        /// The thumbnail must respect any maxwidth and maxheight parameters.
+        /// If this parameter is present, thumbnail_width and thumbnail_height must also be present.
+        /// </summary>
+        [DataMember(Name = "thumbnail_url", EmitDefaultValue = false)]
+        [XmlElement("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+        /// <summary>
+        /// Optional. The width of the optional thumbnail. If this parameter is present, thumbnail_url and thumbnail_height must also be present.
+        /// </summary>
+        [DataMember(Name = "thumbnail_width", EmitDefaultValue = false)]
+        [XmlElement("thumbnail_width")]
+        public string ThumbnailWidth { get; set; }
+
+        /// <summary>
+        /// Optional. The height of the optional thumbnail. If this parameter is present, thumbnail_url and thumbnail_width must also be present.
+        /// </summary>
+        [DataMember(Name = "thumbnail_height", EmitDefaultValue = false)]
+        [XmlElement("thumbnail_height")]
+        public string ThumbnailHeight { get; set; }
+
+        /// <summary>
+        /// For type=photo only. Required. The source URL of the image. Consumers should be able to insert this URL into an <img> element. Only HTTP and HTTPS URLs are valid.
+        /// </summary>
+        [DataMember(Name = "url", EmitDefaultValue = false)]
+        [XmlElement("url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// For type=photo,video,rich only. Required. The width in pixels required.
+        /// </summary>
+        [DataMember(Name = "width", EmitDefaultValue = false)]
+        [XmlElement("width")]
+        public int? Width { get; set; }
+
+        /// <summary>
+        /// For type=photo,video,rich only. Required. The height in pixels required.
+        /// </summary>
+        [DataMember(Name = "height", EmitDefaultValue = false)]
+        [XmlElement("height")]
+        public int? Height { get; set; }
+
+        /// <summary>
+        /// For type=video,rich only. Required. The HTML required to display the resource. The HTML should have no padding or margins. Consumers may wish to load the HTML in an off-domain iframe to avoid XSS vulnerabilities.
+        /// </summary>
+        [DataMember(Name = "html", EmitDefaultValue = false)]
+        [XmlElement("html")]
+        public string Html { get; set; }
+    }
+}
